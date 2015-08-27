32994e8b3ed1f2b39835ed3be52acec31a2a94f8
spotapov <sergiy.potapov@gmail.com>
2015-08-27 13:32:09 +0300

    added run with parameter changed TODO

--- TimeC.py
+++ TimeC.py
@@ -12,7 +12,7 @@ import sys
 #TODO HTML report
 #TODO WBS code corresponds to indicated Phase
 #TODO Check if someone forgot to send a file
-#TODO give a parameter "path to reports" to executable file
+
 
 def RunTests(arg1):
 
@@ -30,12 +30,12 @@ def RunTests(arg1):
         print("\n")
         print(file)
         e = CheckWeekNumberInFile.CheckWeekNumberInFile(ws, week_n)
-        #c = GetWeekTotal.Get_week_total(ws)
+        c = GetWeekTotal.Get_week_total(ws)
         d = FileNameMatch.CheckFileNameMatch(file,week_n)
 
                 #TODO make human readable variables
-        #a = Checkdays.check_days(ws,c)
-        #b = CheckWeek.Check_week(ws,c)
+        a = Checkdays.check_days(ws,c)
+        b = CheckWeek.Check_week(ws,c)
     # print("\n Results for days:\n",a,"\n","Results a week:\n",b)
     #else:
         #print(file, "Seems is not a Timesheet report")
